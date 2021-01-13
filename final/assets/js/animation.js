f6a1db5af5f57b33a4c0865c8734bd27bb938664
Maria Papakonstantinou <maria@hotmail.com>
2021-01-13 16:11:17 +0200

    many updates

---
+++ final/assets/js/animation.js
@@ -0,0 +1,29 @@
+
+$(document).ready(function () {
+    $("#cartBtn").click(function () {
+        $("#cart").slideToggle();
+
+    });
+});
+
+$(document).ready(function () {
+    $("#cartCloseBtn").click(function () {
+        $("#cart").slideToggle();
+    });
+});
+
+
+
+$(document).ready(function () {
+    $("#userModalBtn").click(function () {
+        $("#userModal").slideToggle();
+
+    });
+});
+
+$(document).ready(function () {
+    $("#userModalCloseBtn").click(function () {
+        $("#userModal").slideToggle();
+
+    });
+});
